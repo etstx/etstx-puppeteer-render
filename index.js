a507653f1d7d891c7cc83a13f53843b52af23fa1
Bryan Counts <admin@atpresent.com>
2024-10-22 14:37:58 -0700

    First commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const {scrapeLogic} = require("./scrapeLogic");
+const app = express();
+
+const SERVER_NAME = process.env.SERVER_NAME || "ETS-TX";
+const SERVER_PORT = process.env.SERVER_PORT || 4000;
+
+app.get("/scrape", (req, res) => {
+  scrapeLogic(res);
+});
+
+app.get("/", (req, res) => {
+  res.send(`Puppeteer is running on the ${SERVER_NAME} Server on Port ${SERVER_PORT}.`);
+});
+
+app.listen(4000, () => {
+  console.log(`The ${SERVER_NAME} Server is running on Port ${SERVER_PORT}.`);
+});
---
+++ scrapeLogic.js
@@ -0,0 +1,38 @@
+const puppeteer = require("puppeteer");
+
+const scrapeLogic = async (res) => {
+	// Launch the browser and open a new blank page
+	const browser = await puppeteer.launch();
+	try {
+		const page = await browser.newPage();
+    // throw new Error("This is a test error while running Puppeteer on the server.");
+
+		// Navigate the page to a URL.
+		await page.goto("https://developer.chrome.com/");
+
+		// Set screen size.
+		await page.setViewport({ width: 1080, height: 1024 });
+
+		// Type into search box.
+		await page.locator(".devsite-search-field").fill("automate beyond recorder");
+
+		// Wait and click on first result.
+		await page.locator(".devsite-result-item-link").click();
+
+		// Locate the full title with a unique string.
+		const textSelector = await page.locator("text/Customize and automate").waitHandle();
+		const fullTitle = await textSelector?.evaluate((el) => el.textContent);
+
+		// Print the full title.
+		const logStatement = `Puppeteer scrapeLogic is running on the server. The title of the blog post is ${fullTitle}.`;
+		console.log(logStatement);
+		res.send(logStatement);
+	} catch (e) {
+		console.error(e);
+    res.send(`An error occurred while running Puppeteer on the server. ${e}`);
+	} finally {
+		await browser.close();
+	}
+};
+
+module.exports = { scrapeLogic };
