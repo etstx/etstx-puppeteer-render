5fc5d45f3b895f4f70211cb6511d54cce66f2001
Bryan Counts <admin@atpresent.com>
2024-10-22 18:25:56 -0700

    Refactor response and error messages to include server name and port

--- index.js
+++ index.js
@@ -6,13 +6,13 @@ const SERVER_NAME = process.env.SERVER_NAME || "ETS-TX";
 const SERVER_PORT = process.env.SERVER_PORT || 4000;
 
 app.get("/scrape", (req, res) => {
-  scrapeLogic(res, SERVER_NAME, SERVER_PORT);
+	scrapeLogic(res, SERVER_NAME, SERVER_PORT);
 });
 
 app.get("/", (req, res) => {
-  res.send(`Puppeteer is running on Port ${SERVER_PORT} on the ${SERVER_NAME} Server.`);
+	res.send(`Puppeteer is running on Port ${SERVER_PORT} on the ${SERVER_NAME} Server.`);
 });
 
 app.listen(SERVER_PORT, () => {
-  console.log(`The ${SERVER_NAME} Server is running on Port ${SERVER_PORT}.`);
+	console.log(`The ${SERVER_NAME} Server is running on Port ${SERVER_PORT}.`);
 });
